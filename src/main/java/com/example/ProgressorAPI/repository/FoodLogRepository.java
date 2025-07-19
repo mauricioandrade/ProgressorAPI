17b2880c901b2b1e7f68a28f67cd33877eec3a9a
mauricioandrade <mauricio_andrade@outlook.com>
2025-07-18 22:05:41 -0300

    chore(db): add V2 migration script for food_logs table

---
+++ src/main/java/com/example/ProgressorAPI/repository/FoodLogRepository.java
@@ -0,0 +1,6 @@
+package com.example.ProgressorAPI.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FoodLogRepository extends JpaRepository <FoodLogRepository, Long> {
+}
